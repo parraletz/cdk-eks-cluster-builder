66b4ee9e5446bdcdc51b2cd1bb56d9023a573ca6
Alex Parra <parraletz@gmail.com>
2025-06-09 12:59:04 -0600

    Initial commit

---
+++ test/aws-community-day2025.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCommunityDay2025 from '../lib/aws-community-day2025-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-community-day2025-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCommunityDay2025.AwsCommunityDay2025Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

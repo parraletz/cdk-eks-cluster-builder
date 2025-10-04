0fdcac9fdbd04fe1b356ad3195617152fd63eb46
Alex Parra <parraletz@gmail.com>
2025-10-04 15:29:14 -0600

    chore: Rename Stack

--- bin/eks-builder.ts
+++ bin/eks-builder.ts
@@ -3,10 +3,10 @@ import { configureApp } from "../lib/common/construct-utils/construct-utils";
 import KarpernterConstruct from "../lib/karpenter-construct";
 
 const app = configureApp();
-new KarpernterConstruct(app, "aws-communityday-2025", {
+new KarpernterConstruct(app, "eks-builder-stack", {
   kubernetesVersion: KubernetesVersion.V1_33,
   gitOpsRepo: "https://gitlab.com/parraletz/gitops-zero-to-platform-eng.git",
   gitOpsPath: "bootstrap",
   platformTeamArns: ["arn:aws:iam::664418956352:user/aparra"],
-  bootstrapAppName: "bootstrap-aws-communityday-2025",
+  bootstrapAppName: "bootstrap-eks-builder",
 });
--- lib/eks-builder-stack.ts
+++ lib/eks-builder-stack.ts
@@ -1,8 +1,8 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AwsCommunityDay2025Stack extends cdk.Stack {
+export class EksBuilderStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
